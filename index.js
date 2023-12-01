fd7b6476fce6e56ec948f672e58c8e697d03da04
cinderblock-wall <142337901+cinderblock-wall@users.noreply.github.com>
2023-12-01 14:39:20 -0500

    added functions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // data: array of objs containing pokemons, var for amount of guesses, if/else to match answer
 
-let pokemonList = [
+const pokemonList = [
     {
         name: 'Pikachu',
         imgSrc: 'https://cdn.vox-cdn.com/thumbor/PqAwdNpc7p-5GUCSMxQPehSU-ck=/0x0:1920x1200/1200x800/filters:focal(810x375:1116x681)/cdn.vox-cdn.com/uploads/chorus_image/image/72524797/pikachu_artwork.0.jpg'
@@ -21,6 +21,52 @@ let pokemonList = [
         imgSrc: 'https://www.pcinvasion.com/wp-content/uploads/2023/01/pokemon-scarlet-violet-evolve-voltorb-into-electrode-result.jpg?fit=1200%2C675',
     }
 ]
+// store index of random answer
+const index = getRandomIndex(pokemonList);
 
-const background = document.querySelector('#app')
-console.log(background);
+//store answer
+const answer = pokemonList[index].name;
+
+// creating image in the div and push to html
+const divApp = document.querySelector('#app')
+displayRandomImg(divApp, pokemonList, index);
+const img = document.querySelector("img");
+
+// amount of tries a user has left
+let guessesLeft = 5;
+
+// store user's guess
+let userGuess = window.prompt(`Who's that Pokemon? You have ${guessesLeft} tries left.`);
+
+// while(guessesLeft !== 0){
+//     if (checkAnswer (userGuess, answer)){
+//         window.alert("Correct");
+//         break;
+//     }
+//     guessesLeft --;
+//     userGuess = window.prompt(`Who's in the Pokeball? You have ${guessesLeft} tries left.`);
+// }
+
+// display random image 
+function displayRandomImg (element, arr, index){
+    
+    const img = document.createElement("img");
+
+    img.setAttribute("src", arr[index].imgSrc);
+// need append child inorder to display in the html
+    element.appendChild(img);
+}
+
+// get random index from array
+function getRandomIndex(arr){
+    return Math.floor(Math.random() * arr.length)
+}
+
+// check if user is correct 
+function checkAnswer (userGuess, answer){
+    if(userGuess.toLowerCase() === answer){
+        return window.alert('Correct!');
+    } else {
+        return window.alert("Wrong!");
+    }
+}
